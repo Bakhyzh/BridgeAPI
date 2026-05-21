32bf8d7608a29a201faf2e6c1c91874bb364e14c
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-05-21 12:11:26 +0500

    changing ChatCheck

--- src/main/java/sg/com/petpal/petpal/controller/ChatCheckController.java
+++ src/main/java/sg/com/petpal/petpal/controller/ChatCheckController.java
@@ -1,5 +1,6 @@
 package sg.com.petpal.petpal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,20 +10,25 @@ import java.util.List;
 import java.util.UUID;
 ///Check from Bakhyt
 public class ChatCheckController {
+    @Autowired
+    private ChatMessageController chatMessageService;
+    public ChatCheckController(ChatMessageController chatMessageService){
+        this.chatMessageService=chatMessageService;
+    }
     @RequestMapping
     @GetMapping({"", "/"})
     public ResponseEntity<List<ChatMessage>> findChatCheckAllMessages(@RequestParam(required = false) UUID chatRoomId) {
-        return ResponseEntity.ok(chatMessageService.findChatRoomAllMessages(chatRoomId));
+        return ResponseEntity.ok(chatMessageService.findChatRoomAllMessages(chatRoomId).getBody());
     }
     @RequestMapping
     @GetMapping({"", "/{id}"})
     public ResponseEntity<List<ChatMessage>> findChatCheckIdMessages(@RequestParam(required = false) UUID chatRoomId) {
-        return ResponseEntity.ok(chatMessageService.findChatRoomAllMessages(chatRoomId));
+        return ResponseEntity.ok(chatMessageService.findChatRoomAllMessages(chatRoomId).getBody());
     }
     @RequestMapping
     @PostMapping({"", "/{id}"})
     public ResponseEntity<List<ChatMessage>> CreateChatCheckIdMessages(@RequestParam(required = false) UUID chatRoomId) {
-        return ResponseEntity.ok(chatMessageService.findChatRoomAllMessages(chatRoomId));
+        return ResponseEntity.ok(chatMessageService.findChatRoomAllMessages(chatRoomId).getBody());
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteChatCheckById(@PathVariable UUID id) {
