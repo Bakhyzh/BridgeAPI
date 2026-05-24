d9fd4f7f114cbff2d514c0874fb84f6d67fe55bb
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-05-24 18:11:11 +0500

    exception for ChatCheck

--- src/main/java/sg/com/petpal/petpal/exception/ChatCheckException.java
+++ src/main/java/sg/com/petpal/petpal/exception/ChatCheckException.java
@@ -1,4 +1,7 @@
 package sg.com.petpal.petpal.exception;
 
-public class ChatCheckException {
+public class ChatCheckException extends RuntimeException{
+    public ChatCheckException(Long id) {
+        super("Owner with id '" + id + "' is not a chat room owner.");
+    }
 }
