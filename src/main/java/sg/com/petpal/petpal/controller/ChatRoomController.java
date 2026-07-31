6b90c77b8e880e2abb531f50360405a6c2253bf2
Bakhyt Zharkynbek <zharqynbekov.b@gmail.com>
2026-07-31 23:50:07 +0500

    Update ChatRoomController.java

--- src/main/java/sg/com/petpal/petpal/controller/ChatRoomController.java
+++ src/main/java/sg/com/petpal/petpal/controller/ChatRoomController.java
@@ -52,5 +52,9 @@ public class ChatRoomController {
         chatRoomService.deleteChatRoomById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteChatRoomById(@PathVariable UUID id) {
+        chatRoomService.deleteChatRoomById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     
 }
